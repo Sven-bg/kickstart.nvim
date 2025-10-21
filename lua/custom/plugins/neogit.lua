b80d935cca52e69812a0b784f118b1ede10e727c
Adriano Pallavicino <adriano.pallavicino@toshiba.eu>
2025-10-21 09:13:48 +0100

    Customize neogit

--- lua/custom/plugins/neogit.lua
+++ lua/custom/plugins/neogit.lua
@@ -19,12 +19,24 @@ return {
 
         -- To hide the hints at the top of the status buffer
         -- disable_hint = true,
-
+        auto_refresh = true, -- Automatically refresh status buffer
         -- To explicitly enable the diffview integration (good practice)
         integrations = {
           diffview = true,
+          telescope = true,
+        },
+        commit_editor = {
+          kind = 'split', -- Use split window for commit messages
+          show_staged_diff = true, -- Show staged diff while committing
+          staged_diff_split_kind = 'vsplit', -- Vertical split for diffs
+          spell_check = true, -- Enable spell check in commit messages
+        },
+        status = {
+          show_head_commit_hash = true,
+          recent_commit_count = 10,
         },
 
+        graph_style = 'unicode',
         -- You can add any other option from the documentation here.
         -- For example:
         -- sections = {
